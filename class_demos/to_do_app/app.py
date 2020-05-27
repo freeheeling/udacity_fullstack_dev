ba66f0bc854f517d07d86b7a817f6b0c7dac6623
Scott Regenthal <scottregenthal@gmail.com>
2020-05-27 14:08:07 -0600

    Initialize and migrate database using flask-migrate

--- class_demos/to_do_app/app.py
+++ class_demos/to_do_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scottregenthal@localhost:5432/to_do_app'
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class ToDo(db.Model):
     __tablename__ = 'todos'
@@ -13,8 +16,6 @@ class ToDo(db.Model):
 
     def __repr__(self):
         return f'<To Do {self.id}: {self.description}>'
-
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
