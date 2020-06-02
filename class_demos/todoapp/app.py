768cf827a6d2f9962c1cc161fd5f048673824ddb
Scott Regenthal <scottregenthal@gmail.com>
2020-06-02 15:05:19 -0600

    Create many-to-many order_items association table for order and product

--- class_demos/todoapp/app.py
+++ class_demos/todoapp/app.py
@@ -27,6 +27,21 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo ID: {self.id}, description: {self.description}, completed: {self.completed}>'
 
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(), nullable=False)
+    products = db.relationship('Product', secondary=order_items,
+      backref=db.backref('orders', lazy=True))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
 @app.route('/lists/create', methods=['POST'])
 def create_list():
     error = False
